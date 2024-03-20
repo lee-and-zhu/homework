cd3e131ca485b79c4e38cd7954088fb51e01a32b
lee-and-zhu <164106422+lee-and-zhu@users.noreply.github.com>
2024-03-20 22:29:51 +0800

    Add files via upload

---
+++ xunhuan.py
@@ -0,0 +1,40 @@
+#print('Hello World\n'*10)
+#while语句
+#i=0
+# while i<10 :
+#     print("hello,word")
+#     print(i)
+#     i+=1
+
+# result=0
+# while i<10 :
+#     i += 1
+#     result+=i
+# print(result)
+
+#while语句+else，break
+# while i<10:
+#     i+=1
+#     print("第",i,"次")
+#     break
+# else:
+#     print("该停了")
+
+#for循环一般用于遍历一个集合的元素
+# tice=("我真不想学，这个代码了")
+# for i in range(0,len(tice)):
+#     print(tice[i])
+# else:
+#     print("end")
+
+# tices=["小黄","小蓝","小绿","小紫"]
+# for i in tices:
+#     print(i)
+
+#反转打印，用加号来完成字符串的凭借
+# result=""
+# tice=("我真不想学，这个代码了")
+# for i in tice:
+#     result=i+result
+# print(result)
+
